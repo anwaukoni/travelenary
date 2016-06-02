47eebd10a626d6e16c0b26837e3687a2adf3b132
anwaukoni <anwaukoni@gmail.com>
2016-06-01 17:18:47 -0700

    Filters based on zipCode but not based on date

--- server.js
+++ server.js
@@ -19,85 +19,82 @@ nunjucks.configure('views',{
 
 var meetupApiKey= '45482a6536f5068326310786f80351c';
 var geoCodeApiKey = "AIzaSyDkLWiFHDZ-kgcUKAMkv0wNAiieQZi21ls";
-var meetupURLTemplate = "https://api.meetup.com/2/open_events/?&key=" + meetupApiKey + "&sign=true&photo-host=public&zip={zipcode}&country=UnitedStates&page=100&category=1,5,10,11,20,21,23,30,31,32";
+var meetupURLTemplate = "https://api.meetup.com/2/open_events/?&key=" + meetupApiKey + "&sign=true&photo-host=public&zip={userZip}&country=UnitedStates&page=1000&category=1,5,10,11,20,21,23,30,31,32";
 var zipCode;
 
-// var myFilters = {
-//   //Create userfilters for location
-//   cityState: request( , function(){}),
-// //
-// //
-// //   //Create userfilter for Time period
-//   startTime: function()
-//
-// }
 
 function middlewareCity(req,res,next){
+  console.log('middlewareCity()');
   var state = req.query.state;
   var city = req.query.city;
   //var googleGeoCode = 'https://maps.googleapis.com/maps/api/geocode/json?address='+state+','+'+'+ city +'&key='+geoCodeApiKey;
 
-  if (city !== undefined && state !== undefined){
-    var googleGeoCode = 'https://maps.googleapis.com/maps/api/geocode/json?address={state},+{city}&key={geoCodeApiKey}';
-    googleGeoCode = googleGeoCode.format({state:state, city: city, geoCodeApiKey: geoCodeApiKey});
-    // googleGeoCode = format(googleGeoCode, {state:state, city: city, geoCodeApiKey: geoCodeApiKey});
-
-    request(googleGeoCode, function(error,response,body){
-      if (error) throw error;
-
-      var data = JSON.parse(body);
-
-
-      var results = data.results;
-
-      if (results.length > 0)
-      {
-        var result = results[0];
-
-        var location = result.geometry.location;
-        console.log(JSON.stringify(location, null, 4));
-        req.user_location = location;
-
-      }
-      next();
-
-
-    });
+  if (city === undefined || state === undefined){
+    next();
+    return;
   }
+
+  var googleGeoCode = 'https://maps.googleapis.com/maps/api/geocode/json?address={state},+{city}&key={geoCodeApiKey}';
+  googleGeoCode = googleGeoCode.format({state:state, city: city, geoCodeApiKey: geoCodeApiKey});
+  // googleGeoCode = format(googleGeoCode, {state:state, city: city, geoCodeApiKey: geoCodeApiKey});
+
+  request(googleGeoCode, function(error,response,body){
+    // console.log('middlewareCity(),body:',body);
+    if (error) throw error;
+
+    var data = JSON.parse(body);
+
+
+    var results = data.results;
+
+    if (results.length > 0)
+    {
+      var result = results[0];
+
+      var location = result.geometry.location;
+      // console.log(JSON.stringify(location, null, 4));
+      req.user_location = location;
+
+    }
+    next();
+
+
+  });
+
 
 }
 function middlewareZip(req,res,next){
+  // console.log('middlewareZip()');
+
+  if (req.user_location === undefined){
+
+    next();
+    return;
+  }
 
   var lngtd = req.user_location.lng;
   var latd = req.user_location.lat;
-  if (req.user_location){
-    var googleGeoCode = 'https://maps.googleapis.com/maps/api/geocode/json?latlng={latd},{lngtd}&key={geoCodeApiKey}';
-    googleGeoCode = googleGeoCode.format({lngtd: lngtd, latd: latd, geoCodeApiKey: geoCodeApiKey});
-    request(googleGeoCode, function(error,response,body){
-      if (error) throw error;
-      var data = JSON.parse(body);
-      var results = data.results;
-      if (results.length > 0)
-      {
-        var result = results[0];
-        // console.log(result.address_components);
-        for(var i = 0; i < result.address_components.length; i++){
-          // console.log(result.address_components[i].types[0]);
-          if (result.address_components[i].types[0] === "postal_code"){
-            console.log(result.address_components[i]);
-          }
+  var googleGeoCode = 'https://maps.googleapis.com/maps/api/geocode/json?latlng={latd},{lngtd}&key={geoCodeApiKey}';
+  googleGeoCode = googleGeoCode.format({lngtd: lngtd, latd: latd, geoCodeApiKey: geoCodeApiKey});
+  request(googleGeoCode, function(error,response,body){
+    if (error) throw error;
+    var data = JSON.parse(body);
+    var results = data.results;
+    if (results.length > 0)
+    {
+      var result = results[0];
+      // console.log(result.address_components);
+      for(var i = 0; i < result.address_components.length; i++){
+        // console.log(result.address_components[i].types[0]);
+        if (result.address_components[i].types[0] === "postal_code"){
+          var zip = result.address_components[i].long_name;
         }
-        // var zip = result.geometry.location;
-        // console.log(JSON.stringify(location, null, 4));
-        // req.user_zip = zip;
-
       }
+      req.userZip = zip;
+    }
+    next();
+  });
 
-      next();
-
-
-    });
-  }
 
 }
 
@@ -107,22 +104,35 @@ app.get('/',middlewareCity, middlewareZip, function(req,res, next) {
   // "dallas" will now be stored in `location`; else `location` should be
   // undefined.
   // var startTime = req.query.time;
-  if (req.user_zip !== undefined){
-    var meetupURL = meetupURLTemplate.format({zipcode: zipcode});
-    request(meetupURL,  function(error,response,body){
 
+
+  var start = req.query.startDate;
+  var endDate = req.query.endDate;
+
+  // console.log('app.get("/")');
+  if (req.userZip !== undefined){
+    var userZip = req.userZip;
+
+    var meetupURL = meetupURLTemplate.format({userZip: userZip});
+    request(meetupURL,  function(error,response,body){
       var meetups = [];
+      var betweenDate= [];
       var events = JSON.parse(body);
 
       for(var i = 0; i < events.results.length; i++){
-        meetups.push(events.results[i]);
+        var meetup = events.results[i];
+        meetups.push(meetup);
+
+
       }
-      res.render('index.html', {name:"Gus", logged_in: true, meetups: meetups, location:undefined});
+
+      // console.log(meetups);
+      res.render('index.html', {name:"Gus", logged_in: true, meetups: meetups, city: req.query.city, state: req.query.state, has_results: true});
 
     });
   } else {
 
-    res.render('index.html', {name:"Gus", logged_in: true, meetups: [], location:undefined});
+    res.render('index.html', {name:"Gus", logged_in: true, meetups: [], has_results: false});
 
   }
 
